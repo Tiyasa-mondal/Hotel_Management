e5c9d73e6812ba53bf2bb7c1d2f52f4dd6b5ba1a
Tiyasa-mondal <tiyasamondal00@gmail.com>
2024-09-21 23:41:29 +0530

    Deleted temporary changes

--- home.php
+++ home.php
@@ -263,7 +263,6 @@ if ($usermail == true) {
       </div>
     </div>
   </section>
-  <!-- This is the tempoary changes for testing -->
 
   <section id="thirdsection">
     <h1 class="head"> SPECIAL FACILITY </h1>
